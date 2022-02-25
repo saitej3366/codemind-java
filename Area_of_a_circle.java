dadae635387daf7d5a3ebe8ae125830456387626
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-02-25 15:34:26 +0530

    import java.util.*; class basic{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();          double s=(3.14)*(n*n);         System.out.println(s);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class basic{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+         double s=(3.14)*(n*n);
+        System.out.println(s);
+    }
+}
