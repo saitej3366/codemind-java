59114d1d1baf2e6852d8fe5c871dd02d5339a761
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-02-27 23:06:58 +0530

    import java.util.*; class basic{     public static void main(String args[]){         Scanner s = new Scanner(System.in);        int a=s.nextInt();        int b=s.nextInt();        int d=a+b;         System.out.println(d);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class basic{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+       int a=s.nextInt();
+       int b=s.nextInt();
+       int d=a+b; 
+        System.out.println(d);
+    }
+}
+
