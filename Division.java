f9ac632b02298300d915f5e69b556c4a2d280dfb
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-02-27 22:55:16 +0530

    import java.util.*; class basic{     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int div = s.nextInt();         int dis = s.nextInt();         int d = div/dis;         System.out.println(d);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class basic{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int div = s.nextInt();
+        int dis = s.nextInt();
+        int d = div/dis;
+        System.out.println(d);
+    }
+}
