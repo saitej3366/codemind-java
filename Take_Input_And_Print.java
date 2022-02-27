6ba421197e03f4e4ca12cff2a55cea4c60095eeb
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-02-27 23:01:35 +0530

    import java.util.*; class basic{     public static void main(String args[]){         Scanner s = new Scanner(System.in);        String d=s.nextLine();
            System.out.println(d);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+class basic{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+       String d=s.nextLine();
+        
+        System.out.println(d);
+    }
+}
+
