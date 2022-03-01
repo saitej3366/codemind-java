46aa762f4ab6d6648b6b8b87052beec9b4643eb6
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-03-01 07:27:19 +0530

    import java.util.*; class demo24 {
    	public static void main(String[] args) {
    		// TODO Auto-generated method stub
    		        Scanner sc = new Scanner(System.in);
    		        char c =sc.next().charAt(0);
    		        if(c =='A'||c =='E'||c=='I'||c=='O'||c=='U'){
    		            System.out.println("Vowel");
    		        }
    		        else if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
    		        {
    		            System.out.println("Vowel");
    		        }
    		        else
    		        {
    		            System.out.println("Consonant");
    		        }
    	}
    
    }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class demo24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner sc = new Scanner(System.in);
+		        char c =sc.next().charAt(0);
+		        if(c =='A'||c =='E'||c=='I'||c=='O'||c=='U'){
+		            System.out.println("Vowel");
+		        }
+		        else if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+		        {
+		            System.out.println("Vowel");
+		        }
+		        else
+		        {
+		            System.out.println("Consonant");
+		        }
+	}
+
+}
