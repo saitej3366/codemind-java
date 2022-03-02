8a5e00f43a485986221491694a7771b33d76373a
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-03-02 22:56:26 +0530

    import java.util.*; class ASCII {
    	public static void main(String[] args) {
    		Scanner s = new Scanner(System.in);
    		char alphabet = s.next().charAt(0);
    		System.out.println((int)alphabet);
    	
    
    	}
    
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ASCII {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char alphabet = s.next().charAt(0);
+		System.out.println((int)alphabet);
+	
+
+	}
+
+}
