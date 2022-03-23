b035d659fbf16045fdbad2ac1e4d0a777294bd19
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-03-23 23:28:42 +0530

    import java.util.Scanner; class p{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int n=s.nextInt();
               for(int j=1;j<=n;j++){
                for(int i=j;i<=n;i++){
                    System.out.print(i+" ");
                }
                System.out.println();   
               }
                
                
            
        }
    }

---
+++ Print_the_following_pattern.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+class p{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        
+           for(int j=1;j<=n;j++){
+            for(int i=j;i<=n;i++){
+                System.out.print(i+" ");
+            }
+            System.out.println();   
+           }
+            
+            
+        
+    }
+}
