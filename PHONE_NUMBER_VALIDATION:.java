a056c0e9f04816b7db130a9f1e630e58a147b12e
saitej3366 <84464442+saitej3366@users.noreply.github.com>
2022-03-27 09:54:22 +0530

    import java.util.*; class ab{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String str=sc.nextLine();         int r=str.length();         if(r==10){           System.out.println("Valid");         }else{             System.out.println("Invalid");         }     } }

---
+++ PHONE_NUMBER_VALIDATION:.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+class ab{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int r=str.length();
+        if(r==10){
+          System.out.println("Valid");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
+}
